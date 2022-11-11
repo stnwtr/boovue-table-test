8db7434d426c8e9c745f85fad749306d5908ea26
stnwtr <simon.steinkellner.ss@gmail.com>
2022-11-11 12:47:44 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <table class="table">
+    <tr>
+      <th>hi</th>
+      <th>hi</th>
+      <th>hi</th>
+      <th>hi</th>
+      <th>hi</th>
+    </tr>
+    <tr>
+      <td><button class="btn btn-primary"><i class="bi bi-trash-fill"></i></button></td>
+      <td>hi</td>
+      <td><button class="btn btn-primary"><i class="fa fa-pencil"></i></button></td>
+      <td>hi</td>
+      <td><button class="btn btn-primary">hi</button></td>
+    </tr>
+  </table>
+</template>
+
+<style scoped>
+</style>
