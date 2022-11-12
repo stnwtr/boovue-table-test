21a6467c356faff6a5dac253d97455b99ddde8fb
stnwtr <simon.steinkellner.ss@gmail.com>
2022-11-12 14:46:24 +0100

    fixed by adding thead and tbody

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <table class="table">
-    <tr>
-      <th>hi</th>
-      <th>hi</th>
-      <th>hi</th>
-      <th>hi</th>
-      <th>hi</th>
-    </tr>
-    <tr>
-      <td><button class="btn btn-primary"><i class="bi bi-trash-fill"></i></button></td>
-      <td>hi</td>
-      <td><button class="btn btn-primary"><i class="fa fa-pencil"></i></button></td>
-      <td>hi</td>
-      <td><button class="btn btn-primary">hi</button></td>
-    </tr>
+    <thead>
+      <tr>
+        <th>hi</th>
+        <th>hi</th>
+        <th>hi</th>
+        <th>hi</th>
+        <th>hi</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          <button class="btn btn-primary">
+            <i class="bi bi-trash-fill"></i>
+          </button>
+        </td>
+        <td>hi</td>
+        <td>
+          <button class="btn btn-primary"><i class="fa fa-pencil"></i></button>
+        </td>
+        <td>hi</td>
+        <td><button class="btn btn-primary">hi</button></td>
+      </tr>
+    </tbody>
   </table>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
